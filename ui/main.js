a82f2db66f322e50c2d7782e05a1dde1623c8cb2
LaxmiSathy <30462491+LaxmiSathy@users.noreply.github.com>
2017-08-11 15:27:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ alert('Hi I\'m a JavaScript');
 var element = document.getElementById('main-text');
 element.innerHTML = "New Content from Java Script";
 var img = document.getElementById('madi');
+var marginLeft = 0;
 img.onclick = function() {
-    img.style.marginLeft='100px';
+    function moveRight(){
+        marginLeft = marginLeft + 5;
+        img.style.marginLeft = marginLeft + 'px';
+        
+    }
+    var interval = setInterval(moveRight, 50);
     
 };
