b3d0be3261b525ff6e29611bbdbb928b6b2b3eda
LaxmiSathy <30462491+LaxmiSathy@users.noreply.github.com>
2017-08-11 15:21:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ alert('Hi I\'m a JavaScript');
 // change the text 
 var element = document.getElementById('main-text');
 element.innerHTML = "New Content from Java Script";
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginLeft='100px';
+    
+};
