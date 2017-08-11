c9e63a5a6cbaaff37975ca20df883d0f825a3e0d
LaxmiSathy <30462491+LaxmiSathy@users.noreply.github.com>
2017-08-11 15:13:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,5 @@
 console.log('Loaded!');
 alert('Hi I\'m a JavaScript');
+// change the text 
+var element = document.getElementById('main-text');
+element.innerHTML = "New Content from Java Script";
