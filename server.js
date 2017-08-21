969a4b63777f145fd20eb4715a1e3f6b057d174c
LaxmiSathy <30462491+LaxmiSathy@users.noreply.github.com>
2017-08-21 16:30:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var name=[];
+var names=[];
 app.get('/submit-name/:name', function(req,res) {
     var name = req.params.name;
     
