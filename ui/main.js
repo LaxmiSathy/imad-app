ed06e9fe57d7405e38eb27e06b88a94b7b8e417b
LaxmiSathy <30462491+LaxmiSathy@users.noreply.github.com>
2017-08-21 17:08:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,16 +28,35 @@ button.onclick = function() {
   var name = nameInput.value;
   var submit = document.getElementById('submit_btn');
   submit.onclick = function() {
-    //Make a requset to the server and send list of names
+    //Create request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+                
+            }
+        };
     
     
     //Capture the response and render the list of names
-    var names =['name 1', 'name 2', 'name 3', 'name4'];
+    var names =request.responseText;
+    //JSON to convert string to array
+    names = JSON.parse(names);
     var list ='';
     for (var i=0; i<names.length; i++) {
        list += '<li>' + names[i] + '</li>';
     }
     var sub = document.getElementById('subname');
     sub.innerHTML = list;
+    
+     //Make request to the counter end point
+    request.open('GET', 'http://laxmisathy63.imad.hasura-app.io/submit-name?name=', true);
+    request.send(null);
   };
   
