1f5386bb329784fd5234aefcd0c51e05b10b1c48
LaxmiSathy <30462491+LaxmiSathy@users.noreply.github.com>
2017-08-30 17:40:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,16 +115,16 @@ app.get('/articles/:articleName', function(req, res){
         if(err) {
             res.status(500).send(err.toString());
         } else {
-            if(result.rows.length===0){
+                if(result.rows.length===0){
                 res.status(404).send('Article Not found');
-            }
-            else {
+               }
+               else {
                 var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
                 }
            }
                 
-        }
+    
         
     });
     
