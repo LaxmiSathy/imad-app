ce678867aa408297be12c56d1b0d42b7ae1dea9a
LaxmiSathy <30462491+LaxmiSathy@users.noreply.github.com>
2017-08-31 20:20:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,3 +58,38 @@ button.onclick = function() {
     request.send(null);
   };
   
+  
+  //Username Password login script
+  
+  //Create request object
+  
+  var request = new XMLHttpRequest();
+    
+    //capture the response and store it in variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200){
+                alert('Logged in successfully');
+            } else if (request.status===403){
+                alert('Invalid username or password');
+            } else if (request.status ===500){
+                alert('Server error');
+            }
+        }
+    };
+    
+  
+  var submit = document.getElementById('login');
+  submit.onclick = function(){
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    request.open('POST', 'http://laxmisathy63.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username:username, password:password}));
+  };
+  
+  
+  //Username Password register script
