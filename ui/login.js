00b1173b156fd52fdf5624a164f1c5147ef6fa0d
LaxmiSathy <30462491+LaxmiSathy@users.noreply.github.com>
2017-09-01 09:58:28 +0530

    [imad-console] Updates ui/login.js

--- ui/login.js
+++ ui/login.js
@@ -35,4 +35,37 @@
   };
   
            
-  //Username Password register script
+  //Username Password signup script
+  //Create request object
+  
+  
+  var request = new XMLHttpRequest();
+    
+    //capture the response and store it in variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200){
+                alert('Thanks for signing.');
+                window.location = "/";
+                
+            } else if (request.status===403){
+                alert('Username already exists');
+            } else if (request.status ===500){
+                alert('Server error');
+            }
+        }
+    };
+    
+  
+  var sub = document.getElementById('signup');
+  sub.onclick = function(){
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    
+    request.open('POST', 'http://laxmisathy63.imad.hasura-app.io/create-user', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username:username, password:password}));
+  };
+  
